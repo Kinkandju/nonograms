49645c228ff0cc2e92354589c78f15a725ec5910
Kinkandju <ferretsphilosophy@gmail.com>
2024-02-06 16:45:05 +0300

    init: start nonograms task

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+import { createElement } from './assets/modules/state_of_the_elements.js';
+import { addElement } from './assets/modules/state_of_the_elements.js';
+import { removeClass } from './assets/modules/changing_classes.js';
+import { addClass } from './assets/modules/changing_classes.js';
