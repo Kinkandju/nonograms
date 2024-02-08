c3c90da1fad353ec00cd44854363d5a44ad61f6b
Kinkandju <ferretsphilosophy@gmail.com>
2024-02-08 17:11:59 +0300

    feat: add the logic of toggling classes

--- assets/modules/changing_classes.js
+++ assets/modules/changing_classes.js
@@ -5,10 +5,16 @@ const addClass = (element, someClass) => {
   element.classList.add(someClass);
 };
 
+//* Switches the class at the element
+const toggleClass = (element, someClass) => {
+  element.classList.toggle(someClass);
+}
+
 //* Removes the class from the element
 const removeClass = (element, someClass) => {
   element.classList.remove(someClass);
 };
 
 export { addClass };
+export { toggleClass };
 export { removeClass };
